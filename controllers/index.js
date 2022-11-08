553506f91c37d62b2d86cc3f3439ae14c362d17f
bri <bcamesi@gmail.com>
2022-11-07 21:05:25 -0500

    Rendering individual drink in show view

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,16 @@
+// this whole repo probably has some unnecessary coding and things in it but it WORKS
+
 const express = require('express');
 const drinks = require('../models/drinks');
 const router = express.Router();
 
 
 router.get("/drinks/", (req, res) => {
-    res.render("drinks_index.ejs", {drinks})
+    res.render("drinks_index.ejs", {allDrinks: drinks})
 });
 
-router.get("/drinks/:indexOfFruitsArray", (req, res) => {
-    res.send(drinks[req.params.indexOfDrinksArray]);
+router.get("/drinks/:id", (req, res) => {
+    res.render("drinks_show.ejs", {allDrinks: drinks[req.params.id]})
 });
 
 
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const drinksController = require('./controllers/index');
+const drinks = require("./models/drinks");
 const app = express();
+const router = express.Router(); 
 app.use(express.static(__dirname + '/public'))
 
 app.use(drinksController);
@@ -10,10 +12,20 @@ app.get("/", (req, res) => {
     res.send('Welcome to the Gitpub App!');
 });
 
+// app.get("/drinks/:index", (req, res) => {
+//     res.send(req.params.index);
+// });
+
+app.get("/drinks", (req, res) => {
+    res.render("drinks_index.ejs");
+  });
+
 app.get("/drinks/:id", (req, res) => {
-    res.send(req.params.id);
-});
+    res.render("drinks_show.ejs");
+  });
 
 app.listen(process.env.PORT, () => {
     console.log(`listening on port ${process.env.PORT}`);
 });
+
+module.exports = router;
